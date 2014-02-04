371fb01476b823267ac9e81414b0a512be428366
Jun Sugahara <j.sghr.jp@gmail.com>
2014-02-04 09:41:46 +0900

    bug fix

--- lib/anderson-darling-test/anderson-darling-test.rb
+++ lib/anderson-darling-test/anderson-darling-test.rb
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
 module AndersonDarlingTest
-require File.expand_path("../stats/stats.rb",__FILE__)
-require File.expand_path("../standard-normal-distribution.rb",__FILE__)
+require 'anderson-darling-test/stats/stats'
+require 'anderson-darling-test/standard-normal-distribution'
+include Stats
 
   def anderson_darling_test(samples)
     if samples.size < 5
-      puts "sample busoku"
+      puts "Samples size need to be more than five."
+      puts "See http://en.wikipedia.org/wiki/Anderson%E2%80%93Darling_test"
       exit
     end
-    p normalized = normalize(samples)
+    normalized = normalize(samples)
     ordered = asc(normalized)
     value = calc_anderson_darling_value(ordered)
 
     n = samples.size.to_f
 
-    p value = value * (1.0 + 4/n - 25/(n*n))
+    value = value * (1.0 + 4/n - 25/(n*n))
 
     critical_value = 1.092 # 1.0% Significance
     #critical_value = 0.918 # 2.5% Significance
@@ -33,7 +35,11 @@ require File.expand_path("../standard-normal-distribution.rb",__FILE__)
     samples.each_with_index do |v,i|
       cdf1 = snd.cumlative_distribution(v)
       cdf2 = snd.cumlative_distribution(samples[samples.size-1-i])
-      sum+= (2.0*(i+1)-1.0)*(Math::log(cdf1) + Math.log(1.0-cdf2))
+      begin
+        sum+= (2.0*(i+1)-1.0)*(Math::log(cdf1) + Math.log(1.0-cdf2))
+      rescue Math::DomainError
+        sum += 0
+      end
     end
     n = samples.size.to_f
     return (-n - (sum/n))
