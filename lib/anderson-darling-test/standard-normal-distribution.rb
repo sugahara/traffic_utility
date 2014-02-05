7992c389dd762381b5ed2fdaa580554246d65f00
Jun Sugahara <j.sghr.jp@gmail.com>
2014-02-05 12:55:13 +0900

    change integration method

--- lib/anderson-darling-test/standard-normal-distribution.rb
+++ lib/anderson-darling-test/standard-normal-distribution.rb
@@ -1,9 +1,15 @@
 class StandardNormalDistribution
-require 'integration'
+  @normal_pdf=lambda {|x| (1/Math.sqrt(2*Math::PI))*Math.exp(-(x**2/2))}
 
-  def cumlative_distribution(x)
-    normal_pdf=lambda {|x| (1/Math.sqrt(2*Math::PI))*Math.exp(-(x**2/2))}
-    return Integration.integrate(Integration::MInfinity, x , {:tolerance=>1e-10}, &normal_pdf)
+  def cumlative_distribution(xe)
+    xs = -20
+    n = 100
+    deltax = (xe-xs)*1.0 / n
+    sum = (@normal_pdf.call(xs)+@normal_pdf.call(xe)) / 2.0
+    for  i  in 1..(n-1)
+      sum = sum + @normal_pdf.call(xs+i*deltax)
+    end
+    deltax * sum
   end
 
 end
